dc846805b8ea31a09b68b2942a37eb56d6da264a
zxyuh <omarfarukk108@gmail.com>
2024-06-05 07:28:01 -0400

    refactor: Refactor Game class to improve code readability and remove unused methods

--- Anagrams/Anagrams.java
+++ Anagrams/Anagrams.java
@@ -1,5 +1,4 @@
 package Anagrams;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -7,34 +6,51 @@ import java.awt.event.ActionListener;
 import java.util.*;
 
 
-
 public class Anagrams implements ActionListener{
-   static final String secretWord = "senput";
    JLabel scrambledWord, label;
    JTextField guessField;
    JButton submitButton;
    JLabel resultLabel;
    JTextArea foundWordsArea;
    JLabel scoreLabel;
+   JLabel mistakeLabel;
    JFrame frame;
-   //Set<String> foundWords
+   Game gameObj = new Game();
+   WordChecker wordCheckerObj = new WordChecker();
+
+
+   //initialize variables
+   private char[] userArray;
+   private char[] givenLetters = new char[6];
+   private String generatedLetters;
+   private ArrayList<String> usedWords = new ArrayList<>();
+   private int score = 0;
+   private int mistakes = 0;
+
 
    public Anagrams(){
        //set up the frame
        frame = new JFrame();
        frame.setTitle("ANAGRAMS");
-       frame.setSize(500, 500);   
+       frame.setSize(500, 500);  
        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
        frame.setLayout(new GridLayout(8, 1));
 
 
-       //inintialize components
+       //initialize components
        label = new JLabel("Your Letters Are", SwingConstants.CENTER);
-       scrambledWord = new JLabel(Game.getLetters(), SwingConstants.CENTER);
+
+
+       //sets the generated letters
+       generatedLetters = gameObj.generateString();
+       givenLetters = generatedLetters.toCharArray();
+
+
+       scrambledWord = new JLabel(generatedLetters, SwingConstants.CENTER);
        resultLabel = new JLabel("", SwingConstants.CENTER);
        scoreLabel = new JLabel("Score: 0", SwingConstants.CENTER);
-
-
+       mistakeLabel = new JLabel("Mistakes: 0", SwingConstants.CENTER);
+       
        guessField = new JTextField();
 
 
@@ -54,32 +70,48 @@ public class Anagrams implements ActionListener{
        frame.add(resultLabel);
        frame.add(foundWordsArea);
        frame.add(scoreLabel);
+       frame.add(mistakeLabel);
 
 
-       //frame.pack();
        frame.setVisible(true);
    }
-
-
-  
+ 
    public void actionPerformed(ActionEvent e){
-       String userGuess = guessField.getText().trim().toLowerCase();
-       resultLabel.setText(userGuess);
-       scoreLabel.setText("Score: " + 10);
-       //foundWords.add(userGuess);
-       foundWordsArea.append(userGuess + "\n");
-       guessField.setText("");
+       String userGuess = guessField.getText().trim();
+       userArray = userGuess.toUpperCase().toCharArray();
+       Arrays.sort(userArray);
+       Arrays.sort(givenLetters);
+
+
+       if(gameObj.countMatchingCharacters(userArray, givenLetters) && wordCheckerObj.isValidWord(userGuess) && !gameObj.used(userGuess, usedWords)){
+            resultLabel.setText(userGuess.toUpperCase() + " IS A VALID WORD!");
+            score += 10;
+            usedWords.add(userGuess);
+            scoreLabel.setText("SCORE: " + score);
+            foundWordsArea.append(userGuess.toUpperCase() + "");
+            guessField.setText("");
+        } else if(mistakes == 5){
+            resultLabel.setText("GAME OVER");
+            scoreLabel.setText("YOUR SCORE IS: " + score);
+            mistakeLabel.setText("MISTAKES: " + mistakes);
+            guessField.setEditable(false);
+            submitButton.setEnabled(false);
+        }else if(!(gameObj.countMatchingCharacters(userArray, givenLetters) && wordCheckerObj.isValidWord(userGuess) && !gameObj.used(userGuess, usedWords))){
+            resultLabel.setText("CANT USE THIS WORD!");
+            mistakes += 1;
+            mistakeLabel.setText("MISTAKES: " + mistakes);
+            System.out.println(mistakes);
+        }
    }
-  
 
 
    //create and show GUI
    private static void runGUI(){
        JFrame.setDefaultLookAndFeelDecorated(true);
-
-
        new Anagrams();
    }
+
+
 
 
    public static void main(String[] args) {
@@ -90,5 +122,3 @@ public class Anagrams implements ActionListener{
        });
    }
 }
-
-
--- Anagrams/Game.java
+++ Anagrams/Game.java
@@ -3,60 +3,42 @@ package Anagrams;
 import java.util.ArrayList;
 import java.util.Random;
 
+
 public class Game {
-    private static String[] letters;
-
-    public static boolean valid(char[] userArray, char[] givenLetters, String userGuess, ArrayList<String> usedWords){
-        if(countMatchingCharacters(userArray, givenLetters) && isValidWord(userGuess) && usedBefore(userGuess, usedWords)){
-            System.out.println("WINNER");
-            return true;
-        } else {
-            System.out.println("CANT USE");
-            return false;
-        }
-    }
-
-    //generates the letters to be used
-   public static String[] generateLetters(){
+    //generates a random string of 6 letters
+    public String generateString(){
         Random rand = new Random();
+        String generatedString = "";
         String[] givenLetters = new String[6];
         for(int i = 0; i < 6; i++){
             char randomLetter = (char)(rand.nextInt(90-65+1) + 65);
             givenLetters[i] = String.valueOf(randomLetter);
-            letters = givenLetters;
         }
-        return givenLetters;
+
+
+        for(int i = 0; i < givenLetters.length; i++){
+            generatedString += givenLetters[i] + " ";
+        }
+
+
+
+
+        return generatedString;
     }
 
-    public static String getLetters(){
-        String word;
-        for(String letterString : letters){
-            word = letterString + " ";
-        }
-        return getLetters();
-    }
-     //checks if the word is valid.
-    public static boolean isValidWord(String userGuess){
-        new WordChecker();
-        if(WordChecker.isValidWord(userGuess)){
+
+    //checks if the word has been used before
+    public boolean used (String userGuess, ArrayList<String> words){
+        if(words.contains(userGuess)){
             return true;
         } else {
             return false;
         }
     }
 
-    //checks if the word has been used before
-    public static boolean usedBefore(String userGuess, ArrayList<String> words){
-        for(int i = 0; i < words.size(); i++ ){
-            if(words.get(i) == userGuess){
-                return true;
-            }
-        }
-        return false;
-    }
 
     //checks if the characters given are used
-    public static boolean countMatchingCharacters(char userLetters[], char givenLetters[]) {
+    public boolean countMatchingCharacters(char userLetters[], char givenLetters[]) {
         int count = 0;
         int length = userLetters.length;
         for(int i = 0; i < userLetters.length; i++){
@@ -67,14 +49,11 @@ public class Game {
                 }
             }
         }
-        
+       
         if(count == length){
             return true;
         } else {
             return false;
         }
     }
-
-
-    
 }
